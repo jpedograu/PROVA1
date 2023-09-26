56d0ab1b5a5cc4001191bc88b6b8a2a80581dee6
jpedograu <ultrasenha@gmail.com>
2023-09-26 10:20:33 -0300

    prova de Sergio
    concluida

---
+++ PROVA.py
@@ -0,0 +1,8 @@
+nota1=int(input('digite sua primeira nota:'))       
+nota2=int(input('digite sua segunda nota:'))
+nota3=int(input('digite sua terceira nota:'))
+nota4=int(input('digite sua quarta nota:'))
+media= (nota1 + nota2 + nota3 + nota4) /4
+print ('sua média é',media)
+  
+
